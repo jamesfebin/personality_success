f410023a53b2f38de0f25f6dca1de81ebee69928
azure provisioned user <febin@febinsvm.febinsvm.b2.internal.cloudapp.net>
2016-03-14 07:31:23 +0000

    Personality

--- input/people.tsv.sh
+++ input/people.tsv.sh
@@ -7,4 +7,4 @@ sed '1d' people_data.csv > no_header_people.csv
 
 awk -F, '{$1=++i "\t" $1;}1' OFS=, no_header_people.csv > output_people_data.csv
 
-cut -f1,165,166,168 -d$'\t' output_people_data.csv 
+cut -f1,12,13,15 -d$'\t' output_people_data.csv 
